80fbbf6fefd5b8aa0642232a5491e0865dd0b831
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-06 18:21:56 -0300

    feat: creating del and update for foods

--- src/main/java/com/daniel/menu/controller/FoodController.java
+++ src/main/java/com/daniel/menu/controller/FoodController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,4 +38,10 @@ public class FoodController {
         Food foodData = new Food(data);
         repository.save(foodData);
     }
+
+    @DeleteMapping
+    public void deleteFood(@RequestBody Long id) {
+        repository.deleteById(id);
+    }
+
 }
