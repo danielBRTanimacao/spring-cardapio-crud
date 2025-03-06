60ec9985842b1fad3a61ff4c884069e73f1a5c5a
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-06 18:11:40 -0300

    feat: creating new DTO to save a new food

--- src/main/java/com/daniel/menu/controller/FoodController.java
+++ src/main/java/com/daniel/menu/controller/FoodController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.daniel.menu.food.Food;
+import com.daniel.menu.food.FoodRequestDTO;
 import com.daniel.menu.food.FoodResponseDTO;
 import com.daniel.menu.repository.FoodRepository;
 
@@ -26,5 +30,11 @@ public class FoodController {
             .stream()
             .map(FoodResponseDTO::new).toList();
         return foodList;
+    }
+
+    @PostMapping
+    public void saveNewFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food(data);
+        repository.save(foodData);
     }
 }
---
+++ src/main/java/com/daniel/menu/food/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package com.daniel.menu.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+    
+}
