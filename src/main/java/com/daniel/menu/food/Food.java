a3f20e2a0674f8908a327bc4823a3d18be273f8b
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-06 11:18:50 -0300

    build && feat: new args and new pom.xml

--- src/main/java/com/daniel/menu/food/Food.java
+++ src/main/java/com/daniel/menu/food/Food.java
@@ -5,11 +5,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Table(name = "foods")
-@Entity(name = "foods")
+@Table(name = "food")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Food {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
