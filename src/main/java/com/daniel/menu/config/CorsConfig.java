eb5d0041f9fcb2d4a71e6fda03539b135a994c67
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-08 09:43:18 -0300

    feat: updating cors origin

--- src/main/java/com/daniel/menu/config/CorsConfig.java
+++ src/main/java/com/daniel/menu/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry ) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("http://localhost:5173/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
