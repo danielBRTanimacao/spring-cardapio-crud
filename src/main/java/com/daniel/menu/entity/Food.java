15c099770416de2f398312319f08aad541300995
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-11 06:57:19 -0300

    refactor: refactoring entity Food

--- src/main/java/com/daniel/menu/entity/Food.java
+++ src/main/java/com/daniel/menu/entity/Food.java
@@ -3,7 +3,7 @@ package com.daniel.menu.entity;
 import com.daniel.menu.dto.FoodRequestDTO;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+
 import lombok.*;
 
 @Table(name = "food")
@@ -17,16 +17,8 @@ public class Food {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull(message = "title deve conter o nome do alimento!")
-    @NotBlank(message = "title deve conter o nome do alimento!")
     private String title;
-
-    @NotNull(message = "image deve conter a imagem alimento!")
-    @NotBlank(message = "image deve conter a imagem alimento!")
     private String image;
-
-    @NotNull(message = "price deve conter o preço do alimento!")
     private Integer price;
 
     public Food(FoodRequestDTO data) {
