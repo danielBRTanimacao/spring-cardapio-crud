c92f506cc7fbcaf8515ccfcf83d243c1ec0432c3
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-04-10 11:24:22 -0300

    build: continue studys using postgresql and spring

--- src/main/java/com/daniel/menu/controller/FoodController.java
+++ src/main/java/com/daniel/menu/controller/FoodController.java
@@ -1,12 +1,9 @@
 package com.daniel.menu.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
